d324703ebe2c0093ee7f41b6e6000a9f3483636b
ofirv <ofirvard55@gmail.com>
2020-09-28 12:02:41 +0300

    fixed disabled text color

--- app/src/main/java/com/example/harmonicapracticeassistant/SongActivity.java
+++ app/src/main/java/com/example/harmonicapracticeassistant/SongActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -12,12 +14,17 @@ public class SongActivity extends AppCompatActivity
 {
     private boolean isRecording = false;
     private boolean isEditing = false;
+    private EditText songName;
+    private EditText songTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
+
+        songName = findViewById(R.id.song_title);
+        songTabs = findViewById(R.id.song_edit_text);
 
         if (getIntent().getExtras().getBoolean(Keys.IS_NEW_SONG))
         {
@@ -47,11 +54,17 @@ public class SongActivity extends AppCompatActivity
             {
                 ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.pencil, getTheme()));
                 isEditing = false;
+
+                songName.setEnabled(false);
+                songTabs.setEnabled(false);
             }
             else
             {
                 ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.floppy_disk, getTheme()));
                 isEditing = true;
+
+                songName.setEnabled(true);
+                songTabs.setEnabled(true);
             }
     }
 }
