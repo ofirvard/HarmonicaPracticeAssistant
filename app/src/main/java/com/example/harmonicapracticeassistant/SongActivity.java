18bf5cdf924287ac61f53e832de969002d16259c
ofirv <ofirvard55@gmail.com>
2021-02-08 00:25:52 +0200

    forgot to push

--- app/src/main/java/com/example/harmonicapracticeassistant/SongActivity.java
+++ app/src/main/java/com/example/harmonicapracticeassistant/SongActivity.java
@@ -19,7 +19,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SongActivity extends AppCompatActivity {
+public class SongActivity extends AppCompatActivity
+{
     private boolean isRecording = false;
     private boolean isEditing = false;
     private EditText songName;
@@ -33,7 +34,8 @@ public class SongActivity extends AppCompatActivity {
     private AppSettings settings;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_builder);
 
@@ -44,7 +46,8 @@ public class SongActivity extends AppCompatActivity {
         this.songTabs = findViewById(R.id.song_text);
 
         isNewSong = getIntent().getExtras().getBoolean(Keys.IS_NEW_SONG);
-        if (!isNewSong) {
+        if (!isNewSong)
+        {
             int songPosition = getIntent().getExtras().getInt(Keys.SONG_POSITION);
             song = songs.get(songPosition);
 
@@ -52,7 +55,9 @@ public class SongActivity extends AppCompatActivity {
             songTabs.setText(song.toString());
 
             // TODO: 10/1/2020 load recordings 
-        } else {
+        }
+        else
+        {
             song = new Song();
         }
 
@@ -71,7 +76,8 @@ public class SongActivity extends AppCompatActivity {
         findViewById(R.id.bracketClose).setVisibility(View.GONE);
     }
 
-    private View setKeyboard() {
+    private View setKeyboard()
+    {
         ViewStub stub = findViewById(R.id.layout_stub);
         if (settings.isKeyboardSlim())
             stub.setLayoutResource(R.layout.buttons_slim);
@@ -81,109 +87,91 @@ public class SongActivity extends AppCompatActivity {
         return stub.inflate();
     }
 
-    public void record(View view) {
+    public void record(View view)
+    {
         if (!isEditing)
-            if (isRecording) {
+            if (isRecording)
+            {
                 ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.microphone, getTheme()));
                 isRecording = false;
-            } else {
+            }
+            else
+            {
                 ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.record, getTheme()));
                 isRecording = true;
             }
     }
 
-    public void editSave(View view) {
+    public void editSave(View view)
+    {
         if (!isRecording)
-            // TODO: 1/31/21 seperate to functions
-            if (isEditing)/// TODO: 10/15/2020 debug this
-            {
-                ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.pencil, getTheme()));
-                isEditing = false;
-
-                keyboard.setVisibility(LinearLayout.GONE);
-                findViewById(R.id.backspace).setVisibility(View.GONE);
-                findViewById(R.id.enter).setVisibility(View.GONE);
-                findViewById(R.id.space).setVisibility(View.GONE);
-                findViewById(R.id.bracketOpen).setVisibility(View.GONE);
-                findViewById(R.id.bracketClose).setVisibility(View.GONE);
-                if (settings.isKeyboardSlim())
-                    findViewById(R.id.blowDraw).setVisibility(View.GONE);
-
-                findViewById(R.id.record).setVisibility(View.VISIBLE);
-                findViewById(R.id.zoomIn).setVisibility(View.VISIBLE);
-                findViewById(R.id.zoomOut).setVisibility(View.VISIBLE);
-
-                songName.setEnabled(false);
-            } else {
-                ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.floppy_disk, getTheme()));
-                isEditing = true;
-
-                keyboard.setVisibility(LinearLayout.VISIBLE);
-                findViewById(R.id.backspace).setVisibility(View.VISIBLE);
-                findViewById(R.id.enter).setVisibility(View.VISIBLE);
-                findViewById(R.id.space).setVisibility(View.VISIBLE);
-                findViewById(R.id.bracketOpen).setVisibility(View.VISIBLE);
-                findViewById(R.id.bracketClose).setVisibility(View.VISIBLE);
-                if (settings.isKeyboardSlim()) {
-                    findViewById(R.id.blowDraw).setVisibility(View.VISIBLE);
-                }
-
-                findViewById(R.id.record).setVisibility(View.GONE);
-                findViewById(R.id.zoomIn).setVisibility(View.GONE);
-                findViewById(R.id.zoomOut).setVisibility(View.GONE);
-
-                songName.setEnabled(true);
-            }
+            switchKeyboard((FloatingActionButton) view);
     }
 
-    public void zoomIn(View view) {
-        if (textSize < Keys.MAX_TEXT_SIZE) {
+    public void zoomIn(View view)
+    {
+        if (textSize < Keys.MAX_TEXT_SIZE)
+        {
             textSize++;
             songTabs.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
         }
     }
 
-    public void zoomOut(View view) {
-        if (textSize > Keys.MIN_TEXT_SIZE) {
+    public void zoomOut(View view)
+    {
+        if (textSize > Keys.MIN_TEXT_SIZE)
+        {
             textSize--;
             songTabs.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
         }
     }
 
-    public void blowDraw(View view) {
+    public void blowDraw(View view)
+    {
         if (isEditing)
-            if (isBlow) {
+            if (isBlow)
+            {
                 ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.minus, getTheme()));
                 isBlow = false;
-            } else {
+            }
+            else
+            {
                 ((FloatingActionButton) view).setImageDrawable(getResources().getDrawable(R.drawable.plus, getTheme()));
                 isBlow = true;
             }
     }
 
-    public void space(View view) {
-        if (isEditing) {
+    public void space(View view)
+    {
+        if (isEditing)
+        {
             song.addNote(Keys.NOTE_SPACE);
             songTabs.append(Song.noteTranslator(Keys.NOTE_SPACE));
         }
     }
 
-    public void enter(View view) {
-        if (isEditing) {
+    public void enter(View view)
+    {
+        if (isEditing)
+        {
             song.addNote(Keys.NOTE_ENTER);
             songTabs.append(Song.noteTranslator(Keys.NOTE_ENTER));
         }
     }
 
-    public void backspace(View view) {
-        if (isEditing) {
+    public void backspace(View view)
+    {
+        if (isEditing)
+        {
             song.deleteLastNote();
             songTabs.setText(song.toString());
         }
     }
 
-    public void note(View view) {
-        if (isEditing) {
+    public void note(View view)
+    {
+        if (isEditing)
+        {
             int noteNumber = Integer.parseInt(((TextView) view).getText().toString().replaceAll(" ", ""));
             if (settings.isKeyboardSlim() && !isBlow)
                 noteNumber *= -1;
@@ -195,33 +183,38 @@ public class SongActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {// TODO: 10/30/2020 is !isNewSong dont do discard
+    public void onBackPressed()
+    {// TODO: 10/30/2020 is !isNewSong dont do discard
         // TODO: 10/10/2020 check why this is causing crash
         // TODO: 9/30/2020 stop recording if recording
         if (isNewSong)
-            if (!checkSongNameFree()) {
+            if (!checkSongNameFree())
+            {
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle(R.string.name_in_use);
                 builder.setMessage(R.string.name_in_use_dialog);
 
                 builder.setPositiveButton(R.string.rename, null);
-                builder.setNegativeButton(R.string.discard, new DialogInterface.OnClickListener() {
+                builder.setNegativeButton(R.string.discard, new DialogInterface.OnClickListener()
+                {
                     @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+                    public void onClick(DialogInterface dialogInterface, int i)
+                    {
                         SongActivity.super.onBackPressed();
                     }
                 });
 
                 AlertDialog dialog = builder.create();
                 dialog.show();
-            } else {
+            }
+            else
+            {
                 // TODO: 9/30/2020 add main recording and recordings once there done
 
                 song.setName(songName.getText().toString());
 
                 if (isNewSong)
                     songs.add(song);
-
 
 
                 Intent intent = new Intent();
@@ -231,7 +224,8 @@ public class SongActivity extends AppCompatActivity {
 
 //            super.onBackPressed();
             }
-        else {
+        else
+        {
             Intent intent = new Intent();
             intent.putParcelableArrayListExtra(Keys.SONGS, (ArrayList<? extends Parcelable>) songs);
             setResult(RESULT_OK, intent);
@@ -239,10 +233,58 @@ public class SongActivity extends AppCompatActivity {
         }
     }
 
-    private boolean checkSongNameFree() {
+    private boolean checkSongNameFree()
+    {
         for (Song song : songs)
             if (song.getName().equals(songName.getText().toString()))
                 return false;
         return true;
+    }
+
+    private void switchKeyboard(FloatingActionButton view)
+    {
+        // TODO: 1/31/21 seperate to functions
+        if (isEditing)/// TODO: 10/15/2020 debug this
+        {
+            view.setImageDrawable(getResources().getDrawable(R.drawable.pencil, getTheme()));
+            isEditing = false;
+
+            keyboard.setVisibility(LinearLayout.GONE);
+            findViewById(R.id.backspace).setVisibility(View.GONE);
+            findViewById(R.id.enter).setVisibility(View.GONE);
+            findViewById(R.id.space).setVisibility(View.GONE);
+            findViewById(R.id.bracketOpen).setVisibility(View.GONE);
+            findViewById(R.id.bracketClose).setVisibility(View.GONE);
+            if (settings.isKeyboardSlim())
+                findViewById(R.id.blowDraw).setVisibility(View.GONE);
+
+            findViewById(R.id.record).setVisibility(View.VISIBLE);
+            findViewById(R.id.zoomIn).setVisibility(View.VISIBLE);
+            findViewById(R.id.zoomOut).setVisibility(View.VISIBLE);
+
+            songName.setEnabled(false);
+        }
+        else
+        {
+            view.setImageDrawable(getResources().getDrawable(R.drawable.floppy_disk, getTheme()));
+            isEditing = true;
+
+            keyboard.setVisibility(LinearLayout.VISIBLE);
+            findViewById(R.id.backspace).setVisibility(View.VISIBLE);
+            findViewById(R.id.enter).setVisibility(View.VISIBLE);
+            findViewById(R.id.space).setVisibility(View.VISIBLE);
+            findViewById(R.id.bracketOpen).setVisibility(View.VISIBLE);
+            findViewById(R.id.bracketClose).setVisibility(View.VISIBLE);
+            if (settings.isKeyboardSlim())
+            {
+                findViewById(R.id.blowDraw).setVisibility(View.VISIBLE);
+            }
+
+            findViewById(R.id.record).setVisibility(View.GONE);
+            findViewById(R.id.zoomIn).setVisibility(View.GONE);
+            findViewById(R.id.zoomOut).setVisibility(View.GONE);
+
+            songName.setEnabled(true);
+        }
     }
 }
